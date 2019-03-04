87935bd5b3edf64decd8e1bad56173c8212bd0bd
nosu <tomasz.nosall@gmail.com>
2019-03-04 15:41:41 +0100

    Test annotations

---
+++ spring-demo-annotations/src/co/tomcio/springdemo/TennisCoach.java
@@ -0,0 +1,13 @@
+package co.tomcio.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component("thatSillyCoach")
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice you backhand volley";
+	}
+
+}
